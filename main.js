e5fc9bc63137926e478a7637d06f8306a53c0797
ThomasStachovsky <thegreyraveen@gmail.com>
2021-01-16 20:53:49 +0100

    file-reader prototype

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var http = require('http');
-var express = require('express');
-var multer = require('multer');
+var http = require("http");
+var express = require("express");
+//var multer = require('multer');
 //var upload = multer({ dest: './data' })
 var app = express();
+var fs = require("fs");
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-    res.render('index', { ok:"nie ok"});
+app.get("/", (req, res) => {
+  res.render("index", { ok: "nie ok" });
 });
 
 // app.post('/u', upload.single('file'), (req, res) => {
@@ -19,10 +19,11 @@ app.get('/', (req, res) => {
 //     res.render('index', { ok:"ok" });
 // });
 
-app.post('/codesubmit', (req, res) => {
-    console.log(req.body.code);
-    res.render('index', { ok:"ok" });
+app.post("/codesubmit", (req, res) => {
+  //console.log(req.body.code);
+  username = req.body.code;
+  userdirs = fs.readdirSync("./" + username);
+  res.render("index", { ok: userdirs });
 });
-
 
 http.createServer(app).listen(3000);
