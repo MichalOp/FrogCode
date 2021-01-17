fe014b892d2f6dcc3b63d28e38fef37a1f9eb07c
ThomasStachovsky <thegreyraveen@gmail.com>
2021-01-17 02:06:58 +0100

    file opening works, traversing directories kind of works but only one level down and then breaks

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index", { ok: "nie ok" });
+  res.render("index", { ok: "Type in your name (for example ../../..)" });
 });
 
 // app.post('/u', upload.single('file'), (req, res) => {
@@ -19,11 +19,26 @@ app.get("/", (req, res) => {
 //     res.render('index', { ok:"ok" });
 // });
 
+username = "nouser"; //brilliant
+
 app.post("/codesubmit", (req, res) => {
   //console.log(req.body.code);
   username = req.body.code;
 
-  userdirs = fs.readdirSync("./" + username);
+  userdirs = fs.readdirSync("./" + username, { withFileTypes: true });
+  console.log("Read dir: " + userdirs);
+  res.render("index", { ok: userdirs });
+});
+
+app.get("/file/:name", function (req, res) {
+  var fileName = req.params.name;
+  console.log("Read file: " + fileName);
+  res.sendFile(__dirname + "/" + fileName);
+});
+
+app.get("/dir/:name", function (req, res) {
+  userdirs = fs.readdirSync("./" + req.params.name, { withFileTypes: true });
+  console.log("Read dir: " + userdirs);
   res.render("index", { ok: userdirs });
 });
 
