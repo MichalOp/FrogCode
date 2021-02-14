64b19d1f22d2b90c8c8ac74fa216a72541d581c4
MichalOp <michal.opanowicz@gmail.com>
2021-02-14 12:36:42 +0100

    nicely split into modules

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 var http = require('http');
 var express = require('express');
 const session = require('express-session');
-const spawn = require('node-pty').spawn;
 
 var app = express();
-server = http.createServer(app)
-const io = require('socket.io')(server);
+server = http.createServer(app);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -21,70 +19,10 @@ const sessionParser = session({
 
 app.use(sessionParser);
 
-io.use((socket, next) => {
-    sessionParser(socket.request, {}, next);
-});
+executor = require("./execution.js");
+pseudo_login = require("./pseudologin.js");
 
-app.get('/', (req, res) => {
-    res.render('index', { ok: "nie ok", current_code: "" });
-});
-
-app.get('/term', (req, res) => {
-    res.render('term');
-});
-
-app.post('/login', (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.pwd);
-    if(!req.body.pwd){
-        res.json({success:false});
-        return;
-    }
-    console.log(`Updating session for user ${req.body.name}`);
-    req.session.userId = req.body.name;
-    req.session.save();
-    res.redirect("/");
-});
-
-
-io.on('connection', function (client) {
-
-    console.log("received");
-    const sess = client.request.session;
-    if (!sess || !sess.userId) {
-        console.log('Session is incorrect');
-        return;
-    } else {
-        console.log(`user: ${sess.userId}`)
-    }
-
-    const sh = spawn('docker', ['run', '-it', '--rm',
-        '--mount', `src=${__dirname}/data/${sess.userId},target=/root/${sess.userId},type=bind`,
-        '-w', `/root/${sess.userId}`,
-        'python:3.7',
-        'bash'], {
-        name: 'xterm-color',
-        cols: 80,
-        rows: 30
-    });
-
-    sh.on('data', function (data) {
-        client.send(data);
-    });
-
-    sh.on('exit', function (code) {
-        console.log("shutdown");
-        client.emit('exit', '** the container died **');
-    });
-
-    client.on('message', function (data) {
-        sh.write(data);
-    });
-
-    client.on('disconnect', function (data) {
-        sh.kill();
-        console.log("disconnected");
-    });
-});
+executor(server, sessionParser);
+pseudo_login(app);
 
 server.listen(8099);
