db6d09a48b1e726f950a8da0d389fdb9fbfedaa2
ThomasStachovsky <thegreyraveen@gmail.com>
2021-02-14 15:38:30 +0100

    change names of test users

--- db_init.sql
+++ db_init.sql
@@ -5,8 +5,8 @@ CREATE TABLE users (
     Pwdhash varchar(100)
 );
 
-INSERT INTO users(Displayname,Username,Pwdhash) VALUES ('Orange','trump','63a9f0ea7bb98050796b649e85481845');
+INSERT INTO users(Displayname,Username,Pwdhash) VALUES ('TestUser_One','testuser1','63a9f0ea7bb98050796b649e85481845');
 
-INSERT INTO users(Displayname,Username,Pwdhash) VALUES ('Come on man','biden','63a9f0ea7bb98050796b649e85481845');
+INSERT INTO users(Displayname,Username,Pwdhash) VALUES ('TestUser_Two','testuser2','63a9f0ea7bb98050796b649e85481845');
 
 SELECT * FROM users;
