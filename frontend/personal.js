7b0b87de34794d9ff5e8df2d2c96450a8435b20e
mksyposz <mksyposz@gmail.com>
2021-02-15 07:06:12 +0100

    Password change

--- frontend/personal.js
+++ frontend/personal.js
@@ -5,10 +5,18 @@ function changePassword() {
     if(newpass != cnewpass) {
         document.getElementByID("error").innerHTML = "Passwords don't match"
     }
-    //data = {..}
-    PostAPI({}, "\changePwd", console.log);
+    data = {oldpwdhash: getHash(oldpass), newpwdhash: getHash(newpass)}
+    PostAPI(data, "/changePwd", console.log);
 }
 
 function deleteAccount() {
     PostAPI({}, "/deleteUser", console.log);
+}
+
+function getHash(pswd) {
+    var dg = new Digest.SHA1();
+    var pwdhash = dg.digest(pswd);
+    const hashArray = Array.from(new Uint8Array(pwdhash));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
 }
